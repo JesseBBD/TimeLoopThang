ef837296a68d6c1ed55a8f30ac7bfac8e2302cf9
Jesse van der Merwe <jesse@bbd.co.za>
2023-09-02 17:05:48 +0200

    stuff

--- Assets/Scripts/Hourglass.cs
+++ Assets/Scripts/Hourglass.cs
@@ -4,28 +4,46 @@ using UnityEngine.UI;
 
 public class Hourglass : MonoBehaviour
 {
-    public TMP_Text age;
-    public Image topFill;
-    public Image bottomFill;
-
-    public float roundDuration = 10f;
-    public int totalRounds = 3;
-    int currentRound = 0;
+    [SerializeField] TMP_Text age;
+    [SerializeField] Image topFill, bottomFill, sand;
+    [SerializeField] float maxAge = 100f;
+    [SerializeField] float currentAge = 0f;
+    [SerializeField] float lerpDuration = 2f;
+    
+    float elapsedTime = 0f;
+    float ageModifier = 0f;
+    bool tickTime = false;
 
     void Awake()
     {
-        SetRoundText(totalRounds);
+        topFill.fillAmount = 1f;
+        bottomFill.fillAmount = 0f;
     }
 
-    public void Begin()
+    void Update()
     {
-        ++currentRound;
+        if (tickTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / lerpDuration);
+            float lerpedValue = Mathf.Lerp(currentAge/100f, (currentAge+ageModifier)/100f, t);
 
+            bottomFill.fillAmount = lerpedValue;
+            topFill.fillAmount = 1f - bottomFill.fillAmount;
+
+            sand.fillAmount = 1f;
+            sand.material.mainTextureOffset = new Vector2(0, t);
+            if(t >= 1f){
+                tickTime = false;
+                ageModifier = 0;
+                sand.fillAmount = 0f;
+            }
+        }
     }
 
-    void SetRoundText(int value)
+    public void addAge(float t)
     {
-        age.text = value.ToString();
+        ageModifier = t;
+        tickTime = true;
     }
-
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -4,6 +4,7 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] float playerHealth;
     [SerializeField] const float playerMaxHealth = 10;
+    [SerializeField] Hourglass hourglass;
 
     void Awake()
     {
@@ -11,6 +12,7 @@ public class PlayerManager : MonoBehaviour
     }
 
     public void TakeDamage(float damage){
+        hourglass.addAge(damage);
         playerHealth -= damage;
         if(playerHealth <= 0){
             gameObject.GetComponent<SpriteRenderer>().color = Color.red;
