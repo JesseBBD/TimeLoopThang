58c0efcd1d693f454b8a3568d98027fef47bcaa6
Jesse van der Merwe <jesse@bbd.co.za>
2023-09-02 20:15:45 +0200

    hourglass done

--- Assets/Scripts/Hourglass.cs
+++ Assets/Scripts/Hourglass.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,10 +9,10 @@ public class Hourglass : MonoBehaviour
     [SerializeField] Image topFill, bottomFill, sand;
     [SerializeField] float maxAge = 100f;
     [SerializeField] float currentAge = 0f;
+    [SerializeField] float oldAge = 0f;
     [SerializeField] float lerpDuration = 2f;
-    
+
     float elapsedTime = 0f;
-    float ageModifier = 0f;
     bool tickTime = false;
 
     void Awake()
@@ -26,24 +27,26 @@ public class Hourglass : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             float t = Mathf.Clamp01(elapsedTime / lerpDuration);
-            float lerpedValue = Mathf.Lerp(currentAge/100f, (currentAge+ageModifier)/100f, t);
+            float lerpedValue = Mathf.Lerp(oldAge / 100f, currentAge / 100f, t);
 
             bottomFill.fillAmount = lerpedValue;
             topFill.fillAmount = 1f - bottomFill.fillAmount;
 
             sand.fillAmount = 1f;
             sand.material.mainTextureOffset = new Vector2(0, t);
-            if(t >= 1f){
+
+            if (t >= 1f)
+            {
+                oldAge = currentAge;
                 tickTime = false;
-                ageModifier = 0;
-                sand.fillAmount = 0f;
             }
+            age.text = Mathf.Round(lerpedValue*100f).ToString();
         }
     }
 
-    public void addAge(float t)
+    public void addAge(float t, float a)
     {
-        ageModifier = t;
+        currentAge = a;
         tickTime = true;
     }
 }
